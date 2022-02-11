037641719a86a43a08fa783c5dfdcdd0e9e7f869
raphaelCoelhoDeLima <rcl.lp@hotmail.com>
2022-02-11 17:15:10 -0300

    Ceep_Notas_Alura
    Inicio do estudo de React pela Alura. Sistemas de notas.

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react"
+import "./estilo.css"
+class FormularioCadastro extends Component {
+  constructor(){
+    super()
+    this.titulo = ""
+  }
+  
+  handleMudancaTitulo(evento){
+    this.titulo = evento.target.value
+  }
+  render() {
+    return (
+      <form className="form-cadastro ">
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+          onChange={this.handleMudancaTitulo}
+        />
+        <textarea
+          rows={15}
+          placeholder="Escreva sua nota..."
+          className="form-cadastro_input"
+        />
+        <button className="form-cadastro_input form-cadastro_submit">
+          Criar Nota
+        </button>
+      </form>
+    );
+  }
+}
+
+export default FormularioCadastro;
